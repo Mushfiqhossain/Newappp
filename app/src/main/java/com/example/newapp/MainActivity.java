cf0878397d7b25f560a83e306b24fd4a4a33896d
Hp <mushfiqhossain22@gmail.com>
2022-11-15 01:18:52 +0600

    eh heee

--- app/src/main/java/com/example/newapp/MainActivity.java
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.newapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    CalenderFragment calenderFragment = new CalenderFragment();
+    AlarmFragment alarmFragment = new AlarmFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+                    case R.id.calendar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, calenderFragment).commit();
+
+                    case R.id.alarm:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, alarmFragment).commit();
+
+                    case R.id.settings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsFragment).commit();
+
+                }
+
+            }
+        });
+
     }
 }
