b93cd683e214edfceb46fefcad24d90d36fda3b8
Hp <mushfiqhossain22@gmail.com>
2022-11-16 20:49:25 +0600

    navigation done

--- app/src/main/java/com/example/newapp/MainActivity.java
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -2,26 +2,60 @@ package com.example.newapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.newapp.databinding.ActivityMainBinding;
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     BottomNavigationView bottomNavigationView;
-
-    HomeFragment homeFragment = new HomeFragment();
-    CalenderFragment calenderFragment = new CalenderFragment();
-    AlarmFragment alarmFragment = new AlarmFragment();
-    SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FragmentTransaction homeTrans= getSupportFragmentManager().beginTransaction();
+        homeTrans.replace(R.id.frame_layout,new HomeFragment());
+        homeTrans.commit();
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+            switch (item.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.calendar:
+                    replaceFragment(new CalenderFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.alarm:
+                    replaceFragment(new AlarmFragment());
+            }
+            return true;
+        });
     }
-}
+
+                private void replaceFragment (Fragment fragment){
+
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+
+                }
+            }
